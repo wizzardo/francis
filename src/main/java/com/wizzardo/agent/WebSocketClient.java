5295bc2afb14e661f98a2d2e1a33e769cf3ee197
wizzardo <wizzardobox@gmail.com>
2017-01-19 18:08:25 +0100

    sending some info

--- src/main/java/com/wizzardo/agent/WebSocketClient.java
+++ src/main/java/com/wizzardo/agent/WebSocketClient.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.jar.Manifest;
 
 /**
  * Created by wizzardo on 25/01/17.
@@ -25,8 +26,33 @@ public class WebSocketClient extends SimpleWebSocketClient {
     public WebSocketClient(String url) throws URISyntaxException, IOException {
         super(new Request(url));
         setDaemon(true);
+        setDefaultUncaughtExceptionHandler((t, e) -> e.printStackTrace());
 
-        handlers.put("hello", json -> System.out.println(json));
+        handlers.put("hello", json -> {
+            System.out.println(json);
+
+            JsonObject jsonManifest;
+            JsonObject response = new JsonObject()
+                    .append("command", "setParameters")
+                    .append("params", jsonManifest = new JsonObject());
+            try {
+                String appName = System.getenv("APP_NAME");
+                if (appName != null)
+                    jsonManifest.append("appName", appName);
+
+                Manifest manifest = new Manifest(String.class.getResourceAsStream("/META-INF/MANIFEST.MF"));
+                manifest.getMainAttributes().forEach((k, v) ->
+                        jsonManifest.append(String.valueOf(k), String.valueOf(v))
+                );
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                send(response);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
@@ -58,6 +84,10 @@ public class WebSocketClient extends SimpleWebSocketClient {
         else
             System.out.println("unknown command: " + message.asString());
 
+    }
+
+    public void send(JsonObject json) throws IOException {
+        super.send(json.toString());
     }
 
     @Override
